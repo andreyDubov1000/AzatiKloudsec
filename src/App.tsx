9e6a24d3b11702c2f6002ce49092ff874bb23cd1
Md Shah Ali <shahalihridoy@gmail.com>
2021-06-24 00:11:06 +0600

    loader added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Loader from "@component/atoms/Loader";
 import MuiTheme from "@component/theme/MuiTheme";
 import reduxStore from "@redux/reduxStore";
 import AccountConfirmation from "pages/auth/AccountConfirmation";
@@ -8,7 +9,7 @@ import ResetPassword from "pages/auth/ResetPassword";
 import Signup from "pages/auth/Signup";
 import Dashboard from "pages/Dashboard";
 import Home from "pages/home/Home";
-import React from "react";
+import React, { Suspense } from "react";
 import { NotificationContainer } from "react-notifications";
 import "react-notifications/lib/notifications.css";
 import { Provider as ReduxProvider } from "react-redux";
@@ -19,16 +20,18 @@ const App = () => {
     <ReduxProvider store={reduxStore}>
       <MuiTheme>
         <Router>
-          <Switch>
-            <Route path="/confirm-account" component={AccountConfirmation} />
-            <Route path="/confirm" component={PasswordConfirmation} />
-            <Route path="/signup" component={Signup} />
-            <Route path="/login" component={Login} />
-            <Route path="/resend-otp" component={ForgotPassword} exact />
-            <Route path="/forgot-password" component={ResetPassword} />
-            <Route path="/dashboard" component={Dashboard} />
-            <Route path="/" component={Home} />
-          </Switch>
+          <Suspense fallback={<Loader />}>
+            <Switch>
+              <Route path="/confirm-account" component={AccountConfirmation} />
+              <Route path="/confirm" component={PasswordConfirmation} />
+              <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Login} />
+              <Route path="/resend-otp" component={ForgotPassword} exact />
+              <Route path="/forgot-password" component={ResetPassword} />
+              <Route path="/dashboard" component={Dashboard} />
+              <Route path="/" component={Home} />
+            </Switch>
+          </Suspense>
         </Router>
         <NotificationContainer />
       </MuiTheme>
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,3 +1,4 @@
+import Loader from "@component/atoms/Loader";
 import DashboardLayout from "@component/layouts/DashboardLayout";
 import React from "react";
 export interface DashboardProps {}
@@ -5,9 +6,7 @@ export interface DashboardProps {}
 const Dashboard: React.FC<DashboardProps> = () => {
   return (
     <DashboardLayout>
-      {[...new Array(100)].map((item, ind) => (
-        <h1>shah ali</h1>
-      ))}
+      <Loader />
     </DashboardLayout>
   );
 };
