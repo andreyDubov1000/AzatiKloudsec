5d57ce4151923efa4bcda3913cfc1e89e15a9b7b
German Yanovich <germanyanovich@gmail.com>
2022-02-22 15:10:37 +0300

    add modal popup to ui kit and some refactor

--- src/pages/others/UIKit/UIKit.tsx
+++ src/pages/others/UIKit/UIKit.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { ButtonSimple, InputSearch, RiskButton } from '@component/elements';
+import React, { useState } from 'react';
+import { ButtonSimple, InputSearch, ModalPopUP, RiskButton } from '@component/elements';
 import AvatarEl from '@component/elements/AvatarEl/AvatarEl';
 import ToggleButton from '@component/elements/ToggleButton/ToggleButton';
 
@@ -22,26 +22,51 @@ const UiKitCard = ({ title, children }: UiKitCardPropsType) => {
 };
 
 const UIKit = () => {
+  const [modalActive, setModalActive] = useState(false);
+
+  const buttons = [
+    {
+      title: 'Yes',
+      handler: () => {},
+    },
+    {
+      title: 'No',
+      handler: () => {},
+    },
+  ]
+
   return (
     <div className={s.kit}>
-      <h1>Ui Kit</h1>
-      <UiKitCard title='Risk Button'>
-        <RiskButton />
-      </UiKitCard>
-      <UiKitCard title='Simple Button'>
-        <ButtonSimple title='Accept' />
-      </UiKitCard>
-      <UiKitCard title='Input Search'>
-        <InputSearch />
-      </UiKitCard>
-      <UiKitCard title='Avatar'>
-        <AvatarEl imgSrc='/assets/images/faces/testAvatar.png' size='big' />
-      </UiKitCard>
-      <UiKitCard title='Toggle Button'>
-        <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-          <ToggleButton onChange={(state) => console.log(state)} />
+      <h1 style={{textAlign: 'center'}}>Ui Kit</h1>
+      <div className={s.kitList}>
+        <UiKitCard title='Risk Button'>
+          <RiskButton />
+        </UiKitCard>
+        <UiKitCard title='Simple Button'>
+          <ButtonSimple title='Accept' />
+        </UiKitCard>
+        <UiKitCard title='Input Search'>
+          <InputSearch />
+        </UiKitCard>
+        <UiKitCard title='Modal Popup'>
+        <div>
+          <ModalPopUP
+            modalActive={modalActive}
+            setModalActive={setModalActive}
+            titleOne={'Exceptions'}
+            titleTwo={'Do you want to see all the exceptions?'}
+            buttons={buttons}
+          />
+          <ButtonSimple title='Open Modal' onClick={() => setModalActive(true)} />
         </div>
-      </UiKitCard>
+        </UiKitCard>
+        <UiKitCard title='Avatar'>
+          <AvatarEl imgSrc='/assets/images/faces/testAvatar.png' size='big' />
+        </UiKitCard>
+        <UiKitCard title='Toggle Button'>
+          <ToggleButton onChange={(state) => console.log(state)} />
+        </UiKitCard>
+      </div>
     </div>
   );
 };
